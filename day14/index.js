fda948791ad6921a05cbdd941415849286dd3039
developeralamgirbd <developer.alamgirbd@gmail.com>
2022-09-15 12:55:25 +0600

    first node js project assingment

--- day14/index.js
+++ day14/index.js
@@ -1,22 +1,21 @@
 const http  = require('http');
 const fs    = require('fs');
 const server = http.createServer((req, res)=>{
-    if (req.url === '/'){
 
+    if (req.url === '/'){
         page(res,'pages/home.html')
 
     }else if(req.url === '/sports'){
-
         page(res,'pages/sports.html')
 
     }else if(req.url === '/business'){
-
         page(res,'pages/business.html')
 
     }else if(req.url === '/lifestyle'){
-
         page(res,'pages/lifestyle.html')
 
+    }else{
+        pageNotFound(res)
     }
 });
 
@@ -28,10 +27,15 @@ function page(res, path){
         res.write(readPage);
         res.end();
     }else {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.write('Page Not Found');
-        res.end();
+       pageNotFound(res)
     }
+}
+
+function pageNotFound(res){
+    const readPage = fs.readFileSync('pages/404.html');
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(readPage);
+    res.end();
 }
 
 server.listen(8000);
